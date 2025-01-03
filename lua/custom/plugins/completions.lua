45d6d12e5eece57765bc4a993e85d8b051cfa2d7
Brendan Duke <brendanw.duke@gmail.com>
2025-01-02 20:42:37 -0500

    Add back most keymaps for cmdline, other than Tab and S-Tab

--- lua/custom/plugins/completions.lua
+++ lua/custom/plugins/completions.lua
@@ -7,7 +7,18 @@ return {
     keymap = {
       preset = 'default',
       cmdline = {
+        -- Disable defaults in command-line mode, since binding <Tab> and
+        -- <S-Tab> breaks %:p:h expansion.
         preset = 'none',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+        ['<C-y>'] = { 'select_and_accept' },
+
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
       },
     },
     appearance = {
