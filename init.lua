265b16061085f23692e8ed0539e6fe129f90c075
Brendan <bduke@modular.com>
2025-01-21 15:34:43 +0000

    Remove textwidth=80 for Python files

--- init.lua
+++ init.lua
@@ -178,15 +178,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Set textwidth for Python files.
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
-  callback = function()
-    vim.opt_local.textwidth = 80
-  end,
-  desc = 'Sets textwidth for Python files.',
-})
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
