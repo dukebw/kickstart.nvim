0680319cb3c525ae7d6e9c30289c627cdbb60a13
Brendan <bduke@modular.com>
2025-01-24 21:53:54 +0000

    Enable treesitter with additional vim regex highlighting for MLIR

--- init.lua
+++ init.lua
@@ -867,15 +867,11 @@ require('lazy').setup({
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
-        disable = {
-          -- Disable Treesitter highlighting for MLIR, since the syntax files work better.
-          'mlir',
-        },
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'mlir', 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
