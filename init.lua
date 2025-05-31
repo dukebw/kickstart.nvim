a29d1b95f2dd533db45eb045c46262ebc2218c09
Brendan <bduke@modular.com>
2025-05-31 15:25:25 +0000

    Fix pylsp, ruff and update Lazy lock

--- init.lua
+++ init.lua
@@ -580,13 +580,14 @@ require('lazy').setup({
           settings = {
             pylsp = {
               plugins = {
+                autopep8 = { enabled = false },
                 ruff = {
                   enabled = true,
                   formatEnabled = true,
                   executable = ruff_exe,
                   extendSelect = { 'I' },
                   format = { 'I' },
-                  lineLength = 80,
+                  lineLength = 88,
                   select = { 'ALL' },
                   -- Rules to be ignored by ruff:
                   -- D401: imperative docstring.
@@ -636,6 +637,11 @@ require('lazy').setup({
       }
 
       local lspconfig = require 'lspconfig'
+
+      -- Set up pylsp manually: it is not managed by Mason because we want to
+      -- use the aspect_rules_py MAX venv.
+      local pylsp_cfg = servers.pylsp
+      lspconfig.pylsp.setup(pylsp_cfg)
 
       -- Set up LSP servers unsupported by Mason.
       lspconfig.mlir_lsp_server.setup {
@@ -692,7 +698,7 @@ require('lazy').setup({
       }
 
       vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = { '*.cpp', '*.hpp', '*.c', '*.h', '.cc', '.hh', '.cxx', '.hxx', '*.py', '*.sh', 'BUILD', 'WORKSPACE', '*.bazel', '*.bzl' },
+        pattern = { '*.cpp', '*.hpp', '*.c', '*.h', '.cc', '.hh', '.cxx', '.hxx', '*.sh', 'BUILD', 'WORKSPACE', '*.bazel', '*.bzl' },
         callback = function()
           vim.lsp.buf.format { async = false }
         end,
@@ -722,7 +728,9 @@ require('lazy').setup({
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
+      local ensure_installed = vim.tbl_filter(function(name)
+        return name ~= 'pylsp'
+      end, vim.tbl_keys(servers or {}))
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
@@ -765,7 +773,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, python = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -780,6 +788,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         bzl = { 'bazel' },
+        python = { 'ruff_fix', 'ruff_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
